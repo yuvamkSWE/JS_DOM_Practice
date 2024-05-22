3e76a7b789fde126175d1f1da739179b2a751bb8
Yuvam Kumar <yuvamk.swe@gmail.com>
2024-05-21 21:32:09 -0500

    DOM Practice 2 - Form input

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ for (var i = 0; i < btnElements.length; i++) {
 
 // For adding an event listener to a single element by ID
 // document.getElementById('someId').addEventListener('click', sayHello); --> No loop needed 
+
+
+//document.getElementById("mainFormBtn").onclick = mainFormLogInput();
+//causes the function to run immediately when the script is loaded, and the return value of the function (which is undefined) is assigned as the event handler.
+//To fix this, you need to pass a reference to the mainFormLogInput function without invoking it. Additionally, to prevent the form from submitting and refreshing the page when the button is clicked, you should use event.preventDefault().
+
+
+function mainFormLogInput(){
+    let form = document.forms["mainForm"];
+    let email = form["email"].value;
+    alert(email);
+}
+
+document.getElementById("mainFormBtn").onclick = function(event) {
+    event.preventDefault();
+    mainFormLogInput();
+};
